f46c234aa97860ad682e31afb24f874611cd9c62
umar-Jimoh <umarjimoh@hotmail.com>
2024-05-09 22:14:37 +0800

    fetch chat message based on user input

--- js/script.js
+++ js/script.js
@@ -7,15 +7,28 @@ const baseURL = 'http://localhost:3000'
 function chatMessage(chat, actor) {
   const chatMessage = document.createElement('div')
   chatMessage.classList.add('chat-message')
-  if (actor === 'user') chatMessage.classList.add('user-message')
-  else chatMessage.classList.add('ai-message')
-  chatMessage.innerHTML = `<p>${chat.value}</p>`
+  chatMessage.classList.add(actor === 'user' ? 'user-message' : 'ai-message')
+  chatMessage.innerHTML = `<p>${actor === 'user' ? chat.value : chat.ai}</p>`
   return chatMessage
 }
 
 handleFormSubmit = (e) => {
   e.preventDefault()
   chatWindow.appendChild(chatMessage(input, 'user'))
+  getChatMessage()
+}
+
+async function getChatMessage() {
+  const res = await fetch(`${baseURL}/chats`)
+  let data = await res.json()
+  const userInput = new RegExp(`${input.value}`, 'i')
+  const newData = data.filter((item) => item.user?.match(userInput))
+  input.value = ''
+  if (!newData.length)
+    return chatWindow.appendChild(
+      chatMessage({ ai: "I don't seems to know what you're asking" }, 'ai')
+    )
+  return chatWindow.appendChild(chatMessage(newData[0], 'ai'))
 }
 
 form.addEventListener('submit', handleFormSubmit)
